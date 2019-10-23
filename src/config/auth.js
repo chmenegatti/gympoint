4d61ee36696c912b2a6b56537d5b41ff81f62489
Cesar Menegatti <cesinha51@gmail.com>
2019-10-23 16:01:08 -0300

    Feat: Attentication

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'ed9ca4b3c0e4a8c02eef2b2ba640d11b',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,14 @@
 import { Router } from 'express';
 import userController from './app/controllers/UserController';
+import sessionConstroller from './app/controllers/SessionController';
+import authMiddleware from './app/middleware/auth';
 
 const routes = new Router();
 
 routes.post('/user', userController.store);
+routes.post('/session', sessionConstroller.store);
+
+routes.use(authMiddleware);
+routes.put('/user', userController.upadate);
 
 export default routes;
